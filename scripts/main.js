804828adb11d2a29d4a537d4596fb4f19e11dca8
AJayWalker <65512162+AJayWalker@users.noreply.github.com>
2023-05-03 16:34:12 -0400

    list id change and Obj2Dom changes

--- scripts/main.js
+++ scripts/main.js
@@ -52,7 +52,7 @@ const recipeObj2DOMObj = (recipeObj) => {
   recipeButton.classList.add('btn')
   recipeButton.classList.add('btn-info')
   recipeButton.textContent = recipeObj.word;
-  recipeButton.onclick = searchForBook;
+  recipeButton.onclick = searchNutrition;
   recipeListItem.appendChild(recipeButton);
   return recipeListItem;
 };
